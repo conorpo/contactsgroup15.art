2d61f8228742fcf5c7269626580d938194633bd7
ktorr <103144674+ktorr@users.noreply.github.com>
2023-01-29 16:56:12 -0500

    Create script.js

---
+++ public_html/script.js
@@ -0,0 +1,11 @@
+function ShowSignInForm()
+{
+  var T = document.getElementById("sign-in");
+  T.style.display = "block";
+}
+
+function ShowRegistrationForm()
+{
+  var T = document.getElementById("register");
+  T.style.display = "block";
+}
