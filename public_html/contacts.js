d7c0ec330caf7a9e5414f33faa96da43104c2d4c
Hunter Davis <hunterdavis@knights.ucf.edu>
2023-02-13 13:41:48 -0500

    Created contacts page skeleton

---
+++ public_html/contacts.js
@@ -0,0 +1,33 @@
+// The array of contacts
+let contacts = [jeremy = {firstName:"Jeremy", lastName:"Scott", age:29}];
+// Counts the current top index value of contacts
+let contactsTopIndex = 0;
+
+// Create new contact
+function AddNew() {
+    // Open box to add a person
+    ShowAddBox();
+}
+
+// Log out
+function LogOut() {
+
+}
+
+// Edit current contact info
+function EditContact(index) {
+    // Show the text boxes for each value that is important to us
+
+
+    // Replace info in the html script with that
+}
+
+// Delete current contact info
+function DeleteContact(index) {
+    // Take the index and create a string where it is removed
+
+}
+
+function ShowAddBox() {
+
+}
