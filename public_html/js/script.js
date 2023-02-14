0066505398f9c5f34fedd7d2c13f1d7fe3e2ada9
ktorr <103144674+ktorr@users.noreply.github.com>
2023-02-14 17:08:56 -0500

    Update script.js

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,1 +1,28 @@
+function ShowSignInForm() {
+  document.getElementById("sign in").style.display = "block";
+}
 
+function CloseSignInForm() {
+  document.getElementById("sign in").style.display = "none";
+}
+
+function ShowRegistrationForm() {
+document.getElementById("register").style.display = "block";
+}
+
+function CloseRegistrationForm() {
+  document.getElementById("register").style.display = "none";
+}
+
+function ShowPassTheSame() {
+  var T = document.getElementById("passTheSame");
+
+  if ( document.getElementById("passReg").value !=
+    document.getElementById("confirmReg").value) {
+    T.style.display = "block";
+    document.getElementById("submitReg").disabled = true;
+  } else {
+    T.style.display = "none";
+    document.getElementById("submitReg").disabled = false;
+  }
+}
