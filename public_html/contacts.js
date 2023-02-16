b94d02d0ad93b022806c7c721ae9c8f1407283c1
Hunter Davis <hunterdavis@knights.ucf.edu>
2023-02-16 14:00:34 -0500

    Added a little more to the Contact Box and added distinguishing features to each box.

--- public_html/contacts.js
+++ public_html/contacts.js
@@ -17,9 +17,30 @@ function LogOut() {
 // Edit current contact info
 function EditContact(index) {
     // Show the text boxes for each value that is important to us
-
-
+    document.getElementById("InputFirstNameContact1").style.display = "block";
+    document.getElementById("InputLastNameContact1").style.display = "block";
+    document.getElementById("InputEmailContact1").style.display = "block";
+    document.getElementById("InputPhoneNumberContact1").style.display = "block";
+    
     // Replace info in the html script with that
+    document.getElementById("LabelFirstNameContact1").style.display = "none";
+    document.getElementById("LabelLastNameContact1").style.display = "none";
+    document.getElementById("LabelEmailContact1").style.display = "none";
+    document.getElementById("LabelPhoneNumberContact1").style.display = "none";
+
+    // Hide the edit button
+    document.getElementById("EditContact1").style.display = "none";
+    
+    // Show the save button
+    document.getElementById("SaveContact1").style.display = "block";
+
+}
+
+function SaveContact(index) {
+    // Save the contact at the current index
+
+    // Hide the save button
+    // Show the edit button
 }
 
 // Delete current contact info
